a27f098d75d0434eb4b0afb2273871f8164562c7
fred.c.h.hsu <fred.c.h.hsu@gmail.com>
2015-10-21 02:36:04 +0800

    add config and templates, modify index

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('ROOT', __DIR__);
+
+
+return array(
+    'debug' => true,
+    'mode' => 'development',
+    'templates.path' => __DIR__ . '/templates'
+);
--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
+ini_set("display_errors", "On");
 require 'vendor/autoload.php';
 
-$app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, " . $name;
+$config = require 'config.php';
+$app = new \Slim\Slim($config);
+
+$app->get('/', function() use($app) {
+    // echo "<h1>Hello world</h1";
+  $app->render('html.php');
 });
+
+$app->get('/blog/:alias', function($alias){
+    echo '<h1>Here comes ' . $alias . '</h1>';
+});
+
 $app->run();
