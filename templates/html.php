49801339aeed1a3eb651faf5866983fca7f69394
Fred <fred@barbiedeiMac.local>
2015-10-26 10:06:37 +0800

    add bootstrap and react from CDN

--- templates/html.php
+++ templates/html.php
@@ -3,8 +3,24 @@
     <head>
         <meta charset="UTF-8">
         <title>Slim DEMO</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+        <!-- React -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/react-with-addons.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/react-dom.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
+
     </head>
     <body>
         <?php echo $this->render('index.php'); ?>
+        <script type="text/babel" src="./assets/react/main.js"></script>
+
     </body>
 </html>
--- templates/index.php
+++ templates/index.php
@@ -1,2 +1,4 @@
 <h1>Nick Fury:</h1>
-<code>You think you're the only hero in the world?</code>
+<code>You think you're the only hero in the world? <span class="glyphicon glyphicon-grain"></span> </code>
+
+<div id="example"></div>
